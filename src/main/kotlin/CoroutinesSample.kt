13a9a1b1e74397a3ebf70888b5fa76f464a0ff6e
hiro21 <hiswing8@gmail.com>
2021-06-18 21:33:18 +0900

    add coroutines

--- src/main/kotlin/CoroutinesSample.kt
+++ src/main/kotlin/CoroutinesSample.kt
@@ -1,3 +1,12 @@
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 fun main(args: Array<String>) {
     println("test")
+    runBlocking {
+        launch {
+            println("hello coroutines")
+        }
+    }
+    println("test2")
 }
